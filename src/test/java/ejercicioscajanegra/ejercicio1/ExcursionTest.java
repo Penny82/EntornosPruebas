7f13f857f63cf614012f5caf02532ac6b2521496
Penny82 <sergiobenzal82@gmail.com>
2024-04-16 18:59:01 +0200

    16-04

--- src/test/java/ejercicioscajanegra/ejercicio1/ExcursionTest.java
+++ src/test/java/ejercicioscajanegra/ejercicio1/ExcursionTest.java
@@ -29,12 +29,51 @@ class ExcursionTest {
 
     @Test
     @DisplayName("precio alumno: 100 alumnos")
-    void calcularPrecioAlumno() {
+    void calcularPrecioAlumno1() {
         e = new Excursion(100); // instancio el objeto excursión con 100 alumnos
         assertEquals(65, e.calcularPrecioAlumno());
     }
+    @Test
+    @DisplayName("precio alumno: 50 alumnos")
+    void calcularPrecioAlumno2() {
+        e = new Excursion(50); // instancio el objeto excursión con 100 alumnos
+        assertEquals(70, e.calcularPrecioAlumno());
+    }
+    @Test
+    @DisplayName("precio alumno: 30 alumnos")
+    void calcularPrecioAlumno3() {
+        e = new Excursion(30); // instancio el objeto excursión con 100 alumnos
+        assertEquals(95, e.calcularPrecioAlumno());
+    }
+    @Test
+    @DisplayName("precio alumno: 29 alumnos")
+    void calcularPrecioAlumno4() {
+        e = new Excursion(29); // instancio el objeto excursión con 100 alumnos
+        assertEquals((4000/29), e.calcularPrecioAlumno());
+    }
 
     @Test
-    void calcularCosteExcursion() {
+    @DisplayName("coste total: 100 alumnos")
+    void calcularCosteExcursion1() {
+        e = new Excursion(100);
+        assertEquals(6500, e.calcularCosteExcursion());
+    }
+    @Test
+    @DisplayName("coste total: 50 alumnos")
+    void calcularCosteExcursion2() {
+        e = new Excursion(50);
+        assertEquals(3500, e.calcularCosteExcursion());
+    }
+    @Test
+    @DisplayName("coste total: 30 alumnos")
+    void calcularCosteExcursion3() {
+        e = new Excursion(30);
+        assertEquals(2850, e.calcularCosteExcursion());
+    }
+    @Test
+    @DisplayName("coste total: 29 alumnos")
+    void calcularCosteExcursion4() {
+        e = new Excursion(29);
+        assertEquals((4000), e.calcularCosteExcursion());
     }
 }
